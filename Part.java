4309c6d830c354892e506259959b1e37d08b5631
malik000111 <malickahmar4@gmail.com>
2026-02-19 11:40:47 +0600

    Create Part.java

---
+++ Part.java
@@ -0,0 +1,12 @@
+// Part class represents small components of equipment
+// Example: battery, screen, sensor
+
+public class Part {
+
+    private String partName;   // Name of the part
+
+    // Constructor
+    public Part(String partName) {
+        this.partName = partName;
+    }
+}
